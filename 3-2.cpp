b95f454df6f3679555719c7439870f859076a6b9
makkentoshh <lutskyimakar@gmail.com>
2024-11-05 10:53:56 +0100

    Update 3-2.cpp

--- 3-2.cpp
+++ 3-2.cpp
@@ -1,94 +1,158 @@
-#include <iostream>
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
+#include <cassert>
 #include <limits>
 
+using namespace std;
+
+// Kontener klasy Konten
 template <typename T>
 class Konten {
+    T* data;
+    int capacity;
+    int top;
 public:
-    T* data;            
-    int size;        
-
-    Konten(int n) : size(n) {
-        data = new T[size];
+    Konten(int max_size) : capacity(max_size), top(0) {
+        data = new T[capacity];
     }
 
     ~Konten() {
         delete[] data;
     }
 
+    void push(T value) {
+        assert(top < capacity);
+        data[top++] = value;
+    }
+
+    T pop() {
+        assert(top > 0); 
+        return data[--top];
+    }
+
+    int size() const { return top; }
+
     T& operator[](int index) {
+        assert(index < top);
         return data[index];
     }
+
+    class iterator {
+        Konten& kontener;
+        int index;
+    public:
+        iterator(Konten& k, int start_index = 0) : kontener(k), index(start_index) {}
+
+        iterator& operator++() {
+            ++index;
+            return *this;
+        }
+
+        T& operator*() const {
+            return kontener.data[index];
+        }
+
+        bool operator!=(const iterator& other) const {
+            return index != other.index;
+        }
+    };
+
+    iterator begin() { return iterator(*this, 0); }
+    iterator end() { return iterator(*this, top); }
 };
 
+// Klasa pomiary, przechowująca przed i po pomiary
+template <typename T>
+class pomiary {
+public:
+    Konten<T> przed;
+    Konten<T> po;
+
+    pomiary(int size) : przed(size), po(size) {}
+};
+
+// Funkcja do generowania losowych wartości double
+double randdouble() {
+    return rand() / (double(RAND_MAX) + 1) * 100;
+}
+
+// Klasa Kalkulator
 template <typename T>
 class Kalkulator {
 public:
-    static void NajmOdleglosc(
-        Konten<T>*& pierwszy,
-        Konten<T>*& drugi,
-        Konten<Konten<T>*>& punkty,
-        int liczbaPunktow
+    static void testtStudenta(
+        typename Konten<pomiary<T>>::iterator &start,
+        typename Konten<pomiary<T>>::iterator &end
     ) {
-        double min_odleglosc = std::numeric_limits<double>::max();
+        typename Konten<pomiary<T>>::iterator min_pair = start;
+        double min_T = std::numeric_limits<double>::max();
 
-        for (int i = 0; i < liczbaPunktow; ++i) {
-            for (int j = i + 1; j < liczbaPunktow; ++j) {
-                double odleglosc = obliczOdleglosc(*punkty[i], *punkty[j]);
-                if (odleglosc < min_odleglosc) {
-                    min_odleglosc = odleglosc;
-                    pierwszy = punkty[i];
-                    drugi = punkty[j];
-                }
+        for (int i = 0; start != end; ++start, ++i) {
+            pomiary<T>& pom = *start;
+            int n = pom.przed.size();
+            
+            double D_mean = 0;
+            for (int j = 0; j < n; ++j) {
+                D_mean += (pom.przed[j] - pom.po[j]);
+            }
+            D_mean /= n;
+
+            double S_squared = 0;
+            for (int j = 0; j < n; ++j) {
+                double diff = (pom.przed[j] - pom.po[j]) - D_mean;
+                S_squared += diff * diff;
+            }
+            double S = std::sqrt(S_squared / (n - 1));
+            double T = D_mean / (S / std::sqrt(n));
+
+            if (std::abs(T) < min_T) {
+                min_T = std::abs(T);
+                min_pair = start;
             }
         }
-    }
 
-private:
-    static double obliczOdleglosc(const Konten<T>& p1, const Konten<T>& p2) {
-        double suma = 0;
-        for (int i = 0; i < p1.size; ++i) {
-            suma += std::pow(p1.data[i] - p2.data[i], 2);
-        }
-        return std::sqrt(suma);
+        start = min_pair;
     }
 };
 
 int main() {
     srand(static_cast<unsigned>(time(0)));
 
-    const int m = 7;  
-    const int n = 6;  
+    // Tworzymy kontener typu Konten przechowujący pomiary<double>
+    Konten<pomiary<double>> measurements(10);
 
-    Konten<Konten<double>*> punkty(m);
+    // Generowanie par serii wartości losowych i dodawanie ich do kontenera
+    for (int i = 0; i < 7; ++i) {
+        int series_length = 5 + rand() % 5;
+        pomiary<double> series(series_length);
 
-    for (int i = 0; i < m; ++i) {
-        punkty[i] = new Konten<double>(n);
-        for (int j = 0; j < n; ++j) {
-            punkty[i]->data[j] = static_cast<double>(rand()) / RAND_MAX * 10.0;
+        for (int j = 0; j < series_length; ++j) {
+            series.przed.push(randdouble());
+            series.po.push(randdouble());
         }
+
+        measurements.push(series);
     }
 
-    Konten<double>* pierwszy = nullptr;
-    Konten<double>* drugi = nullptr;
-    Kalkulator<double>::NajmOdleglosc(pierwszy, drugi, punkty, m);
+    // Tworzenie obiektu Kalkulator i wywołanie metody testtStudenta
+    auto start = measurements.begin();
+    auto end = measurements.end();
 
-    std::cout << "Najmniejsza odleglosc jest miedzy punktami:\n";
-    std::cout << "Punkt 1: ";
-    for (int i = 0; i < n; ++i) {
-        std::cout << pierwszy->data[i] << " ";
-    }
-    std::cout << "\nPunkt 2: ";
-    for (int i = 0; i < n; ++i) {
-        std::cout << drugi->data[i] << " ";
-    }
-    std::cout << std::endl;
+    Kalkulator<double>::testtStudenta(start, end);
 
-    for (int i = 0; i < m; ++i) {
-        delete punkty[i];
+    // Wyświetlanie wyników
+    cout << "Para serii o najniższej wartości T: " << endl;
+    pomiary<double>& result = *start;
+    cout << "Przed: ";
+    for (int i = 0; i < result.przed.size(); ++i) {
+        cout << result.przed[i] << " ";
     }
+    cout << "\nPo: ";
+    for (int i = 0; i < result.po.size(); ++i) {
+        cout << result.po[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
